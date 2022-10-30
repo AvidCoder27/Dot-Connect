6f98d2ac00b8e536afd91fa1c461f368e48f7760
AvidCoder27 <gilireeves@gmail.com>
2022-10-30 18:52:58 -0400

    added utils

---
+++ utils.js
@@ -0,0 +1,38 @@
+setCellValue = (xyPair, value) => board[xyPair.y][xyPair.x] = value;
+getCellValue = (xyPair) => board[xyPair.y][xyPair.x];
+
+function addVector(xy1, xy2) {
+  return {x: xy1.x + xy2.x, y: xy1.y + xy2.y};
+}
+function subtractVector(xy1, xy2) {
+  return {x: xy1.x - xy2.x, y: xy1.y - xy2.y};
+}
+function areVectorsEqual(xy1, xy2) {
+  return xy1.x === xy2.x && xy1.y === xy2.y;
+}
+
+function screenSpace(n){
+    return  (n+0.5) * CELL_SIZE;
+}
+
+function getColorOfCell(x, y, l){
+    switch (l[y][x]) {
+      case 0:
+        return EMPTY_COLOR;
+      case 1:
+        return WALL_COLOR;
+      case 2:
+        return PLAYER_COLOR;
+      case 3:
+        return BAD_COLOR;
+      case 8:
+        return START_COLOR;
+      case 9:
+        return END_COLOR;
+    }
+}
+
+function checkForFullBoard(){
+    for (let y = 0; y < boardHeight; y++) for (let x = 0; x < boardWidth; x++) if (!board[y][x]) return false;
+    return true;
+}
