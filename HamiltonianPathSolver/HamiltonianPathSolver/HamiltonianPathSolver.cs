8c6ee51dc544a06f2562f5d69aaa2c9f3123bb85
AvidCoder27 <gilireeves@gmail.com>
2022-11-25 18:37:48 -0500

    more solution status support, refactored main()
    Moved part of main into another method to handle everything. Solves a string and returns an instance of solution.

--- HamiltonianPathSolver/HamiltonianPathSolver/HamiltonianPathSolver.cs
+++ HamiltonianPathSolver/HamiltonianPathSolver/HamiltonianPathSolver.cs
@@ -4,17 +4,51 @@
     {
         static Board? originalBoard;
 
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
             if (args == null || args.Length < 1) throw new Exception("No path specified in args!");
             string path = args[0];
             string content = File.ReadAllText(path);
 
             Console.WriteLine("\nStarting solver on file " + path);
+            Solution solution = SolveJsonString(content);
+
+            Console.WriteLine();
+            switch (solution.status)
+            {
+                case Solution.Status.Success:
+                    Console.WriteLine(solution.stepsAsString);
+                    break;
+                case Solution.Status.NoSolution:
+                    Console.WriteLine("Board has no solution");
+                    break;
+                case Solution.Status.InvalidBoard:
+                    Console.WriteLine("JSON was not a valid board");
+                    break;
+                case Solution.Status.Unsolved:
+                default:
+                    Console.WriteLine("SOMETHING WENT WRONG. The board was not solved");
+                    break;
+            }
+        }
+
+        public static Solution SolveJsonString(string jsonString)
+        {
+            Solution finalSolution = new ();
 
             // create board
-            originalBoard = Newtonsoft.Json.JsonConvert.DeserializeObject<Board>(content);
-            if (originalBoard == null || !originalBoard.IsValid()) return -1;
+            try
+            {
+                originalBoard = Newtonsoft.Json.JsonConvert.DeserializeObject<Board>(jsonString);
+                if (originalBoard == null || !originalBoard.IsValid())
+                    throw new Newtonsoft.Json.JsonReaderException();
+            }
+            catch (Newtonsoft.Json.JsonReaderException)
+            {
+                finalSolution.status = Solution.Status.InvalidBoard;
+                return finalSolution;
+            }
+
             Console.WriteLine(originalBoard);
 
             // setup parallel options
@@ -27,17 +61,21 @@
 
             try
             {
-                Parallel.For(0, 12, po, index =>
+                Parallel.For(0, 24, po, index =>
                 {
                     Thread thisThread = Thread.CurrentThread;
-                    thisThread.Name = "t" + index;
-                    Console.Write($"{thisThread.Name}, ");
+                    thisThread.Name = "Thread" + index;
 
                     Solution solution = originalBoard.GetCopy().Solve(index, po.CancellationToken);
-                    Console.WriteLine($"\n{thisThread.Name} found {solution}");
+                    finalSolution = solution;
+
+                    // converts the char array to a string
+                    string priorityString = new (Board.GetPrioritizationFromIndex(index));
+                    //Console.WriteLine($"\n\n{thisThread.Name} used {priorityString} to find {solution}");
+                    Console.WriteLine($"\n{thisThread.Name} finished in {solution.executionTimeMilliseconds}ms with priority of {priorityString}");
 
                     cts.Cancel();
-                    Console.WriteLine("Cancelled token source. ");
+                    Console.WriteLine("Cancelled token source.");
 
                 });
             }
@@ -50,7 +88,7 @@
                 cts.Dispose();
             }
 
-            return 0;
+            return finalSolution;
         }
     }
 }
