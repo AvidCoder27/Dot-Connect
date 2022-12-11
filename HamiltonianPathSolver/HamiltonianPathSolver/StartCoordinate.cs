4609167e937385c50a853235c0ac44f79152f75b
AvidCoder27 <gilireeves@gmail.com>
2022-12-11 09:36:28 -0500

    Create StartCoordinate.cs

---
+++ HamiltonianPathSolver/HamiltonianPathSolver/StartCoordinate.cs
@@ -0,0 +1,21 @@
+﻿
+namespace HamiltonianPathSolver
+{
+    public class StartCoordinate
+    {
+        // This class is just for storing the XY pair for the start
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public StartCoordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", x, y);
+        }
+    }
+}
